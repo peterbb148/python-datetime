af04ce5a8a372386ba12c635331b745bd6b606a7
Peter Birkholm-Buch <peterbb@gmail.com>
2020-07-25 22:22:44 +0200

    Updated

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from datetime import datetime
 
 datestring = "2020-04-24T09:31:14.824+0000"
-date_obj = datetime.strptime(datestring, "%Y-%m-%dT%H:%M:%S.%f+%z")
+date_obj = datetime.strptime(datestring, "%Y-%m-%dT%H:%M:%S.%f%z")
+print(date_obj)
+print(date_obj.strftime('%Y%m%d-%H%m%S'))
